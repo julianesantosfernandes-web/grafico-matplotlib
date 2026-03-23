7c46dfe1311739ba6ab62e82ce1527bb88cc0cf2
julianesantosfernandes-web <juliane.santos.fernandes@escola.pr.gov.br>
2026-03-23 10:47:22 +0000

    criacao grafico

--- grafico.py
+++ grafico.py
@@ -1,1 +1,8 @@
-
+import matplotlib.pyplot as plt
+meses = ['Janeiro','Fevereiro','Março','Abril','Maio','junho']
+vendas = [200,300,340,305,360,400]
+plt.plot(meses, vendas)
+plt.title('Vendas no Primeiro Semestre')
+plt.xlabel('Meses')
+plt.ylabel('Vendas')
+plt.savefig("grafico_linhas.png")
